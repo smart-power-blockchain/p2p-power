938552975d68c39837e521032b8424628e306141
Akhil <akhiljns@gmail.com>
2019-03-05 23:24:07 +0530

    some more code cleaning

--- frontend blockchain/blockchain_client/blockchain_client.py
+++ frontend blockchain/blockchain_client/blockchain_client.py
@@ -1,13 +1,10 @@
 from collections import OrderedDict
-
 import binascii
-
 import Crypto
 import Crypto.Random
 from Crypto.Hash import SHA
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
-
 import requests
 from flask import Flask, jsonify, request, render_template
 
