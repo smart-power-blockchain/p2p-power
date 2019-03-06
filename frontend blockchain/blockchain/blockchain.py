3161d9805ec47bd7a370695d90aceacd542e0b97
Akhil <akhiljns@gmail.com>
2019-03-07 01:38:54 +0530

    linting errors resolved

--- frontend blockchain/blockchain/blockchain.py
+++ frontend blockchain/blockchain/blockchain.py
@@ -23,21 +23,20 @@ MINING_DIFFICULTY = 2
 class Blockchain:
 
     def __init__(self):
-        
+
         self.transactions = []
         self.chain = []
         self.nodes = set()
-        #Generate random number to be used as node_id
+        # Generate random number to be used as node_id
         self.node_id = str(uuid4()).replace('-', '')
-        #Create genesis block
+        # Create genesis block
         self.create_block(0, '00')
-
 
     def register_node(self, node_url):
         """
         Add a new node to the list of nodes
         """
-        #Checking node_url has valid format
+        # Checking node_url has valid format
         parsed_url = urlparse(node_url)
         if parsed_url.netloc:
             self.nodes.add(parsed_url.netloc)
@@ -46,7 +45,6 @@ class Blockchain:
             self.nodes.add(parsed_url.path)
         else:
             raise ValueError('Invalid URL')
-
 
     def verify_transaction_signature(self, sender_address, signature, transaction):
         """
@@ -58,38 +56,37 @@ class Blockchain:
         h = SHA.new(str(transaction).encode('utf8'))
         return verifier.verify(h, binascii.unhexlify(signature))
 
-
     def submit_transaction(self, sender_address, recipient_address, value, signature):
         """
         Add a transaction to transactions array if the signature verified
         """
-        transaction = OrderedDict({'sender_address': sender_address, 
-                                    'recipient_address': recipient_address,
-                                    'value': value})
+        transaction = OrderedDict({'sender_address': sender_address,
+                                   'recipient_address': recipient_address,
+                                   'value': value})
 
-        #Reward for mining a block
+        # Reward for mining a block
         if sender_address == MINING_SENDER:
             self.transactions.append(transaction)
             return len(self.chain) + 1
-        #Manages transactions from wallet to another wallet
+        # Manages transactions from wallet to another wallet
         else:
-            transaction_verification = self.verify_transaction_signature(sender_address, signature, transaction)
+            transaction_verification = self.verify_transaction_signature(
+                sender_address, signature, transaction)
             if transaction_verification:
                 self.transactions.append(transaction)
                 return len(self.chain) + 1
             else:
                 return False
 
-
     def create_block(self, nonce, previous_hash):
         """
         Add a block of transactions to the blockchain
         """
         block = {'block_number': len(self.chain) + 1,
-                'timestamp': time(),
-                'transactions': self.transactions,
-                'nonce': nonce,
-                'previous_hash': previous_hash}
+                 'timestamp': time(),
+                 'transactions': self.transactions,
+                 'nonce': nonce,
+                 'previous_hash': previous_hash}
 
         # Reset the current list of transactions
         self.transactions = []
@@ -97,16 +94,14 @@ class Blockchain:
         self.chain.append(block)
         return block
 
-
     def hash(self, block):
         """
         Create a SHA-256 hash of a block
         """
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
-        
-        return hashlib.sha256(block_string).hexdigest()
 
+        return hashlib.sha256(block_string).hexdigest()
 
     def proof_of_work(self):
         """
@@ -121,7 +116,6 @@ class Blockchain:
 
         return nonce
 
-
     def valid_proof(self, transactions, last_hash, nonce, difficulty=MINING_DIFFICULTY):
         """
         Check if a hash value satisfies the mining conditions. This function is used within the proof_of_work function.
@@ -129,7 +123,6 @@ class Blockchain:
         guess = (str(transactions)+str(last_hash)+str(nonce)).encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:difficulty] == '0'*difficulty
-
 
     def valid_chain(self, chain):
         """
@@ -148,8 +141,10 @@ class Blockchain:
             # Delete the reward transaction
             transactions = block['transactions'][:-1]
             # Need to make sure that the dictionary is ordered. Otherwise we'll get a different hash
-            transaction_elements = ['sender_address', 'recipient_address', 'value']
-            transactions = [OrderedDict((k, transaction[k]) for k in transaction_elements) for transaction in transactions]
+            transaction_elements = [
+                'sender_address', 'recipient_address', 'value']
+            transactions = [OrderedDict(
+                (k, transaction[k]) for k in transaction_elements) for transaction in transactions]
 
             if not self.valid_proof(transactions, block['previous_hash'], block['nonce'], MINING_DIFFICULTY):
                 return False
@@ -191,6 +186,7 @@ class Blockchain:
 
         return False
 
+
 # Instantiate the Node
 app = Flask(__name__)
 CORS(app)
@@ -198,14 +194,15 @@ CORS(app)
 # Instantiate the Blockchain
 blockchain = Blockchain()
 
+
 @app.route('/')
 def index():
     return render_template('./index.html')
 
+
 @app.route('/configure')
 def configure():
     return render_template('./configure.html')
-
 
 
 @app.route('/transactions/new', methods=['POST'])
@@ -217,14 +214,17 @@ def new_transaction():
     if not all(k in values for k in required):
         return 'Missing values', 400
     # Create a new Transaction
-    transaction_result = blockchain.submit_transaction(values['sender_address'], values['recipient_address'], values['amount'], values['signature'])
+    transaction_result = blockchain.submit_transaction(
+        values['sender_address'], values['recipient_address'], values['amount'], values['signature'])
 
     if transaction_result == False:
         response = {'message': 'Invalid Transaction!'}
         return jsonify(response), 406
     else:
-        response = {'message': 'Transaction will be added to Block '+ str(transaction_result)}
+        response = {
+            'message': 'Transaction will be added to Block ' + str(transaction_result)}
         return jsonify(response), 201
+
 
 @app.route('/transactions/get', methods=['GET'])
 def get_transactions():
@@ -234,6 +234,7 @@ def get_transactions():
     response = {'transactions': transactions}
     return jsonify(response), 200
 
+
 @app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
@@ -242,6 +243,7 @@ def full_chain():
     }
     return jsonify(response), 200
 
+
 @app.route('/mine', methods=['GET'])
 def mine():
     # We run the proof of work algorithm to get the next proof...
@@ -249,7 +251,8 @@ def mine():
     nonce = blockchain.proof_of_work()
 
     # We must receive a reward for finding the proof.
-    blockchain.submit_transaction(sender_address=MINING_SENDER, recipient_address=blockchain.node_id, value=MINING_REWARD, signature="")
+    blockchain.submit_transaction(
+        sender_address=MINING_SENDER, recipient_address=blockchain.node_id, value=MINING_REWARD, signature="")
 
     # Forge the new Block by adding it to the chain
     previous_hash = blockchain.hash(last_block)
@@ -263,7 +266,6 @@ def mine():
         'previous_hash': block['previous_hash'],
     }
     return jsonify(response), 200
-
 
 
 @app.route('/nodes/register', methods=['POST'])
@@ -308,21 +310,13 @@ def get_nodes():
     return jsonify(response), 200
 
 
-
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    parser.add_argument('-p', '--port', default=5000,
+                        type=int, help='port to listen on')
     args = parser.parse_args()
     port = args.port
 
-    app.run(host='127.0.0.1', port=port , debug=True)
-
-
-
-
-
-
-
-
+    app.run(host='127.0.0.1', port=port, debug=True)
